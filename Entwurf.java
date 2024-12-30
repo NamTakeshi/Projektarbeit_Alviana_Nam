48b7c10b3005a90c563be783b17abd02ef79b7df
Nam Phan <namphn.2@gmail.com>
2024-12-30 17:14:51 +0100

    für Entwurf und Screenshots erstellt

---
+++ Entwurf.java
@@ -0,0 +1,146 @@
+// Diese Klasse ist nur für den Entwurf und Screenshots gedacht. Wird später gelöscht. 
+public class Entwurf {
+    
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Superheroes vs. IT-Giant 1.0\r\n" + //
+                        "(1) Create Superhero\r\n" + //
+                        "(2) Show Selected Superhero\r\n" + //
+                        "(3) List all Superheroes\r\n" + //
+                        "(4) Delete Superhero\r\n" + //
+                        "(5) Start Raid\r\n" + //
+                        "(6) Quit\r\n" + //
+                        "Please choose a number between 1 and 6: \r\n" + //
+                        "");
+        System.out.println("");
+
+        // Spieler wählt (1)
+        System.out.println("1"); 
+        System.out.println("Give your new Superhero a name: ");
+        System.out.println("Spiderman");
+        System.out.println("Your new Superhero 'Spiderman' was created.");
+        System.out.println("");
+
+        // Spieler wählt (2)
+        System.out.println("Your Superheroes: ");
+        System.out.println("(1) Batman\n(2) Spiderman\n(3) Superman");
+        System.out.println("Choose one number to see more about your Superhero: ");
+        System.out.println("");
+        
+            // Valide Input
+            System.out.println(2);
+            System.out.println("Spiderman: ");
+            System.out.println("Name: Spiderman\nLives: 2\nEnergyPointsCurrent: 20\nExperiencePoints: 0\nReadyToFight: yes\n");
+            System.out.println("");
+
+            // Invalide Input
+            System.out.println("Your Superheroes: ");
+            System.out.println("(1) Batman\n(2) Spiderman\n(3) Superman");
+            System.out.println("Choose one number to see more about your Superhero: ");
+            System.out.println("Zwei");
+            System.out.println("");
+            System.out.println("Your input was invalid. Please try again.");
+            System.out.println("");
+            System.out.println("Your Superheroes: ");
+            System.out.println("(1) Batman\n(2) Spiderman\n(3) Superman");
+            System.out.println("Choose one number to see more about your Superhero: ");
+            System.out.println("");
+
+        // Spieler wählt (3)
+        System.out.println("All of your Superheroes: ");
+        System.out.println("");
+        System.out.println("Batman: ");
+        System.out.println("Name: Batman\n" + //
+                        "Lives: 2\n" + //
+                        "EnergyPointsCurrent: 20\n" + //
+                        "ExperiencePoints: 0\n" + //
+                        "ReadyToFight: yes\n" + //
+                        "");
+        System.out.println("Name: Spiderman\n" + //
+                        "Lives: 2\n" + //
+                        "EnergyPointsCurrent: 20\n" + //
+                        "ExperiencePoints: 0\n" + //
+                        "ReadyToFight: yes\n" + //
+                        "");
+        System.out.println("Name: Superman\n" + //
+                        "Lives: 2\n" + //
+                        "EnergyPointsCurrent: 20\n" + //
+                        "ExperiencePoints: 0\n" + //
+                        "ReadyToFight: yes\n" + //
+                        ""); 
+        
+        // Spieler wählt (4) 
+        System.out.println("Your Superheroes: ");
+        System.out.println("(1) Batman\n(2) Spiderman\n(3) Superman");
+        System.out.println("Choose which Superhero to delete: ");
+        System.out.println("");
+            // Valide Input
+            System.out.println("1");
+            System.out.println("Batman was deleted.");
+            // Invalide Input
+            System.out.println("4");
+            System.out.println("Your input was invalid. Please try again.");
+            System.out.println("Your Superheroes: ");
+        System.out.println("(1) Batman\n(2) Spiderman\n(3) Superman");
+        System.out.println("Choose which Superhero to delete: ");
+        System.out.println("");
+
+        // Spieler wählt (5)
+        System.out.println("------ Raid Menu ------");
+        System.out.println("(1) Explore");
+        System.out.println("(2) Dance");
+        System.out.println("(3) Quit Raid");
+        System.out.println("");
+        System.out.println("Please choose a number between 1 and 3: ");
+        System.out.println(""); 
+
+        // Invalide Input
+        System.out.println(7); 
+        System.out.println("Your input was invalid, please choose a number between 1 and 3."); 
+        System.out.println(""); 
+        System.out.println("------ Raid Menu ------");
+        System.out.println("(1) Explore");
+        System.out.println("(2) Dance");
+        System.out.println("(3) Quit Raid");
+        System.out.println("");
+        System.out.println("Please choose a number between 1 and 3: ");
+        System.out.println(""); 
+
+        // Spieler wählt (6)
+        System.out.println("The game ends.");
+        System.out.println(""); 
+
+        // 7) Spieler gibt ungültige Eingabe im Hauptmenü ein. 
+        System.out.println("");
+        System.out.println("Superheroes vs. IT-Giant 1.0\r\n" + //
+                        "(1) Create Superhero\r\n" + //
+                        "(2) Show Selected Superhero\r\n" + //
+                        "(3) List all Superheroes\r\n" + //
+                        "(4) Delete Superhero\r\n" + //
+                        "(5) Start Raid\r\n" + //
+                        "(6) Quit\r\n" + //
+                        "Please choose a number between 1 and 6: \r\n" + //
+                        "");
+        System.out.println("");
+
+        System.out.println(7);
+        System.out.println("Your input was invalid, please choose a number between 1 and 6.");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Superheroes vs. IT-Giant 1.0\r\n" + //
+                        "(1) Create Superhero\r\n" + //
+                        "(2) Show Selected Superhero\r\n" + //
+                        "(3) List all Superheroes\r\n" + //
+                        "(4) Delete Superhero\r\n" + //
+                        "(5) Start Raid\r\n" + //
+                        "(6) Quit\r\n" + //
+                        "Please choose a number between 1 and 6: \r\n" + //
+                        "");
+        System.out.println("");
+
+
+
+
+
+    }
+}
